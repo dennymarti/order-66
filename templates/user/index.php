95478dbc66677805944a5d712cef68f8b578dcdb
Denny Marti <denny.marti@outlook.com>
2022-04-24 18:17:37 +0200

    Added account view

--- templates/user/index.php
+++ templates/user/index.php
@@ -1,6 +1,27 @@
-<div class="account-wrapper">
-    <p><?= $user->firstname ?></p>
-    <p><?= $user->name ?></p>
-    <p><?= $user->username ?></p>
-    <a title="Löschen" href="/user/delete?id=<?= $user->id; ?>">Löschen</a>
+<div class="profile-wrapper">
+    <div class="box">
+        <h1 class="form-title"><?= $heading; ?></h1>
+    </div>
+
+    <div class="box">
+        <div class="form-row">
+            <input class="input" value="<?= $user->firstname ?>" readonly>
+        </div>
+
+        <div class="form-row">
+            <input class="input" value="<?= $user->name ?>" readonly>
+        </div>
+
+        <div class="form-row">
+            <input class="input" value="<?= $user->username ?>" readonly>
+        </div>
+
+        <p class="form-text">
+            Don't need account anymore? <a class="link" href="/user/delete?id=<?= $user->id; ?>">Delete</a>
+        </p>
+    </div>
+
+    <div class="form-submit">
+        <button class="submit" type="button" disabled>Edit</button>
+    </div>
 </div>
