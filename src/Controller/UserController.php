9620fd23b8eca2184e0b09b94541f12f42be71c8
nri129640 <nri129640@iet-gibb.ch>
2022-04-25 09:48:59 +0200

    added the generation of the username. Completed

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use App\Service\AuthenticationService;
 use App\View\View;
+use http\Client\Curl\User;
 
 /**
  * Siehe Dokumentation im DefaultController.
  */
 class UserController
 {
+
+    private $namesArray = array("4-LOM", "Secura", "Ackbar", "Thrawn", "Ahsoka", "Anakin", "Ventress", "Sing", "Organa", "Offee", "Shan", "Ben", "Bib", "Biggs", "Boba", "Bossk", "Brakiss", "C-3PO", "Bane", "Cade", "Ming", "Rex", "Jax", "Chewbacca", "Cody", "Dooku", "Krayt", "Maul", "Nihilus", "Vader", "Rendar", "Dengar", "Durge", "Palpatine", "Kun", "Marek", "Madine", "Dodonna", "Grievous", "Veers", "Pellaeon", "Moff", "Greedo", "Solo", "IG 88", "Jabba", "Jacen", "Jaina", "Jango", "Jarael", "Jerec", "C'Baoth", "Ki-Adi-Mundi", "Kanos", "Fisto", "Katarn", "Durron", "Lando", "Luke", "Luminara", "Lumiya", "Windu", "Mara", "Vao", "Daala", "Anor", "Kenobi", "Padmé", "Koon", "Vizsla", "Xizor", "Leia", "PROXY", "Jinn", "Vos", "R2-D2", "Kota", "Revan", "Shan", "Opress", "Sebulba", "Shaak", "Shmi", "Karrde", "Ulic", "Marr", "Watto", "Wedge", "Yoda", "Zam", "Zayne", "Zuckuss", );
+
     public function index()
     {
         if (AuthenticationService::isAuthenticated()) {
@@ -18,7 +22,7 @@ class UserController
 
             $view = new View('user/index');
 
-            $view->isLoggedIn = isset($_SESSION['id']);
+            $view->isLoggedIn = true;
 
             $view->title = 'Konto';
             $view->heading = 'Konto';
@@ -43,9 +47,15 @@ class UserController
             header('Location: /user/signup');
         }
         else {
+            $userRepository = new UserRepository();
             $firstName = $_POST['firstname'];
             $name = $_POST['name'];
-            $username = strtolower("$firstName" . "$name");
+            do {
+                $nameIndex = rand(0, 93);
+                $number = rand(10, 99);
+                $randName = $this->namesArray[$nameIndex];
+                $username = str_replace(' ', '', strtolower("$randName" . "$number"));
+            }while(!empty($userRepository->readByUsername($username)));
             $password = $_POST['password'];
 
             $userRepository = new UserRepository();
